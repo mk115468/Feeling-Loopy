5db121750b3b86570f4835c2ff0a36ebd8e3a20f
mk115468 <91619190+mk115468@users.noreply.github.com>
2021-11-18 12:09:51 -0500

    finished exercise 4

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ for i in range(value+1):
 print(sum)"""
 
 
-#EXCERCISE 3
+"""#EXCERCISE 3
 highest=0
 for i in range(3):
   total_score=0
@@ -30,4 +30,24 @@ for i in range(3):
     highest = total_score
 
   print("total score is: " , total_score)
-print("highest score is: " , highest)
+print("highest score is: " , highest)"""
+
+"""#EXERCISE 4
+import random
+hp=100
+complete=False
+while not complete: 
+ cont=input("play another round? (y/n)")
+ if cont=="y":
+   damage=random.randint(0,100)
+   hp-=damage
+   if hp<=0:
+     print("Game Over")
+     complete=True
+   else:
+      print("you were by damaged by",damage, "and your HP is now",hp)
+ elif cont =="n":
+   complete=True
+ else:
+   print("enter valid input")"""
+  
